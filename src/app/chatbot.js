fca0000680519f4a0e3c4079808daab4751d4f6e
alwayshappy <reihan@localhost.localdomain>
2025-12-26 01:05:20 +0100

    add new feature, chatbot

---
+++ src/app/chatbot.js
@@ -0,0 +1,58 @@
+import ollama from "ollama";
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+const userHistory = new Map();
+
+const dataForResponse = [
+  {
+    text: "[REPORT]",
+    response:
+      "Oke saya paham anda pasti takut ingin melapor langsung apa yang anda alami di sekolah, anda bisa membuat laporan tersebut di form ini : https://message.testings/hehe",
+  },
+];
+
+wss.on("connection", (socket) => {
+  userHistory.set(socket, []);
+
+  socket.on("message", async (data) => {
+    // cek dulu siapa tau meminta data pasti
+    const message = data.toString();
+    for (const tp of dataForResponse) {
+      if (message.includes(tp.text)) {
+        socket.send(tp.response);
+        return;
+      }
+    }
+
+    const history = userHistory.get(socket);
+    history.push({ role: "user", content: data.toString() });
+
+    const res = await ollama.chat({
+      model: "llama3.1:8b-instruct-q4_K_M",
+      messages: [
+        { role: "system", content: "kamu pendengar empatik" },
+        {
+          role: "system",
+          content: "yang mengobrol bersamamu adalah korban bullying",
+        },
+        ...history,
+      ],
+      stream: true,
+    });
+
+    let fullResponse = "";
+
+    for await (const part of res) {
+      fullResponse += part.message?.content || "";
+    }
+
+    // kirim setelah selesai
+    socket.send(fullResponse);
+
+    // simpan ke history biar percakapan lanjut
+    history.push({ role: "assistant", content: fullResponse });
+  });
+});
+
+export default wss;
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import { web } from "./app/web.js";
 import http from "http";
 import { logger } from "./app/logging.js";
+import wss from "./app/chatbot.js"; // untuk di intrepreter
 dotenv.config();
 const port = process.env.PORT || 6000;
 const host = process.env.HOST || "https://api-hacktown.rusnandapurnama.com";
